8091109c30588a9f0921023b1e5fd1cd277656e9
Ivan Khudyakov <ivanch83ya@yandex.ru>
2022-05-15 22:15:02 +0300

    Restart from lesson 3

---
+++ Snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        //Методы класса Point
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
