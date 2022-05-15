4130ae25b718cd4b3d0ade5d1a2f7eb581320547
Ivan Khudyakov <ivanch83ya@yandex.ru>
2022-05-15 22:27:22 +0300

    add constructor for class Point

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -12,6 +12,14 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+
         //Методы класса Point
         public void Draw()
         {
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,27 +6,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
-
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw(); 
+            
+            Point p2 = new Point(6, 9, '#');
+            p2.Draw();
 
             Console.ReadKey();
 
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
     }
 }
