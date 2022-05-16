efee615049ecdab2cd9eb2ea591f2e20f1148341
Ivan Khudyakov <ivanch83ya@yandex.ru>
2022-05-16 21:53:05 +0300

    some change

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -7,31 +10,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw(); 
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Draw(); 
             
-            Point p2 = new Point(6, 9, '#');
-            p2.Draw();
+            //Point p2 = new Point(6, 9, '#');
+            //p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.Remove(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
             Console.ReadKey();
 
