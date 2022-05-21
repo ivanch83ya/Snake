917b01ecc297ba7fe74985a8af7b08466f2ed48c
Ivan Khudyakov <ivanch83ya@yandex.ru>
2022-05-21 21:23:24 +0300

    Add FoodCreator class

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -47,6 +47,11 @@ namespace Snake
             }
 
         }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,7 +13,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(120, 50);
+            Console.SetBufferSize(120, 30);
 
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
@@ -29,15 +29,31 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while(true)
             {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(300);
+                Thread.Sleep(100);
                 snake.Move();
             }
 
